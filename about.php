0e8bfc92994d82b06400fe58fa138fcabf0b268c
yumokanzaki <evil_louis@hotmail.com>
2016-12-08 20:10:50 -0600

    Add files via upload

--- about.php
+++ about.php
@@ -4,31 +4,12 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Attack on Titan Database - About</title>
 	<link rel="stylesheet" href="css/style.css" type="text/css" charset="utf-8" />	
-</head>
+	</head>
 <body>
-	<span id="background"></span>
-	<div id="page">
-		<div id="sidebar">
-			<div id="logo">
-				<a href="index.php">Welcome to the <br/> <em>Attack on Titan Database</em></a>
-			</div> <!-- /#logo -->
-			<ul id="navigation">
-				<li class="selected"><a href="about.php">About</a></li>
-				<li><a href="index.php">Illustrations</a></li>
-				<li><a href="images.php">Selected Images</a></li>
-				<li><a href="contact-us.php">Contact Us</a></li>
-				<li><a href="blog.php">Blog</a></li>
-			</ul> <!-- /#navigation -->
-			<ul id="connect">
-				<li><a href="http://facebook.com/" target="_blank" class="facebook"></a></li>
-				<li><a href="http://twitter.com/" target="_blank" class="twitter"></a></li>
-				<li><a href="" class="link-us"></a></li>
-			</ul> <!-- /#connect -->
-			<div class="footer">
-				&copy; Copyright &copy; 2016.<br/>
-				<a href="index.php">Company name</a> all rights reserved.
-			</div> <!-- /.footer -->
-		</div> <!-- /#sidebar -->		
+	<?php	
+	include "phpheader.php"
+	?>
+<!-- /#sidebar -->		
 		<div id="contents">
 			<span class="header"></span>
 			<h3>About</h3>
--- index.php
+++ index.php
@@ -6,29 +6,10 @@
 	<link rel="stylesheet" href="css/style.css" type="text/css" charset="utf-8" />	
 </head>
 <body>
-	<span id="background"></span>
-	<div id="page">
-		<div id="sidebar">
-			<div id="logo">
-				<a href="index.php">Welcome to the <br/> <em>Attack on Titan Database</em></a>
-			</div> <!-- /#logo -->
-			<ul id="navigation">
-				<li><a href="about.php">About</a></li>
-				<li class="selected"><a href="index.php">Illustrations</a></li>
-				<li><a href="images.php">Selected Images</a></li>
-				<li><a href="contact-us.php">Contact Us</a></li>
-				<li><a href="blog.php">Blog</a></li>
-			</ul> <!-- /#navigation -->
-			<ul id="connect">
-				<li><a href="http://facebook.com/" target="_blank" class="facebook"></a></li>
-				<li><a href="http://twitter.com/" target="_blank" class="twitter"></a></li>
-				<li><a href="" class="link-us"></a></li>
-			</ul> <!-- /#connect -->
-			<div class="footer">
-				&copy; Copyright &copy; 2016.<br/>
-				<a href="index.php">Company name</a> all rights reserved.
-			</div> <!-- /.footer -->
-		</div> <!-- /#sidebar -->		
+	<?php
+	include "phpheader.php"
+	?>	
+<!-- /#sidebar -->		
 		<div id="contents">
 			<span class="header"></span>
 			<ul class="images">
